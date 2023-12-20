25a312c1e6d69d0f9f2f3eb444dba8044603843f
Anastasiya Trembach <nastia.trembach@gmail.com>
2023-12-20 19:47:00 +0200

    move MessageBroker staff

---
+++ Core/MessageBroker/MessageBrokerBaseWithProvider.cs
@@ -0,0 +1,20 @@
+﻿using MessageBroker.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provider.Core
+{
+    public abstract class MessageBrokerBase<TProviderSettings> : MessageBrokerBase 
+        where TProviderSettings : class
+    {
+        public TProviderSettings ProviderSettings { get; }
+
+        protected MessageBrokerBase(MessageBrokerSettings settings, TProviderSettings providerSettings) : base(settings)
+        {
+            ProviderSettings = providerSettings ?? throw new ArgumentNullException(nameof(providerSettings));
+        }
+    }
+}
