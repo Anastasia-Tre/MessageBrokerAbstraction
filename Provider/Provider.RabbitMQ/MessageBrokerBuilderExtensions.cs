be043a50e9dc71b3145ff1b5aa160223be2ecabe
Anastasiya Trembach <nastia.trembach@gmail.com>
2024-05-16 19:01:47 +0300

    add provider for rabbit mq

---
+++ Provider/Provider.RabbitMQ/MessageBrokerBuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿using MessageBroker.Core.MessageBroker;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Provider.RabbitMQ
+{
+    public static class MessageBrokerBuilderExtensions
+    {
+        public static MessageBrokerBuilder WithProviderRedis(
+            this MessageBrokerBuilder mbb,
+            Action<RabbitMQMessageBrokerSettings> configure)
+        {
+            ArgumentNullException.ThrowIfNull(mbb);
+            ArgumentNullException.ThrowIfNull(configure);
+
+            var providerSettings = new RabbitMQMessageBrokerSettings();
+            configure(providerSettings);
+
+            return mbb.WithProvider(settings =>
+                new RabbitMQMessageBroker(settings, providerSettings));
+        }
+    }
+}
