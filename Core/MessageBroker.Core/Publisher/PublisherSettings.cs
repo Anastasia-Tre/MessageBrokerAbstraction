05d2f5bbfa75a3e11e8a4aa1a8f057494d5dda2c
Anastasiya Trembach <nastia.trembach@gmail.com>
2024-05-17 13:01:19 +0300

    add properties to publisher settings

--- Core/MessageBroker.Core/Publisher/PublisherSettings.cs
+++ Core/MessageBroker.Core/Publisher/PublisherSettings.cs
@@ -7,4 +7,19 @@ public class PublisherSettings
     public string? DefaultTopic { get; set; }
 
     public TimeSpan? ExpiredTimeout { get; set; }
+
+    public IDictionary<string, object> Properties { get; protected set; }
+
+    protected PublisherSettings()
+    {
+        Properties = new Dictionary<string, object>();
+    }
+
+    public T GetOrDefault<T>(string key, T defaultValue)
+    {
+        if (Properties.TryGetValue(key, out var value))
+            return (T)value;
+        
+        return defaultValue;
+    }
 }
